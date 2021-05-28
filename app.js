31ea780c83d989d8f1588afc52fb77992ff8bb66
Ellie Banks <elliebanks@ellies-mbp.home>
2021-05-28 10:55:09 -0500

    update. fixed grammar & function loop

--- app.js
+++ app.js
@@ -5,45 +5,65 @@
     console.log(friends[i])
 }*/
 
-let friends = ["Liz", "Devon", "Bethany", "Taylor", "Jessica"];
-let body = document.body;
-
-
-
-
-window.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
     let sing = document.createElement("button");
     let buttonText = document.createTextNode("click me to sing that dumb song");
     sing.appendChild(buttonText);
-    document.body.appendChild(sing);
-    
+
+
     sing.addEventListener("click", function () {
+        let friends = ["Liz", "Devon", "Bethany", "Taylor", "Jessica"];
+
+       /* for (i = 0; i < friends.length; i++) { */
+        let newFriendDiv = document.createElement("div");
+        
         friends.forEach(function (name, index) {
-            let newFriendDiv = document.createElement("div");
-            let newFriendHeader = document.createElement("h3");
-            let newFriendParagraph = document.createElement("p");
-            
-            newFriendDiv.setAttribute("class", "friend");
-            newFriendHeader.innerText = name;
-            newFriendDiv.appendChild(newFriendHeader);
-            newFriendHeader.appendChild(newFriendParagraph);
-            
-            
-            for (let f = 99; f > 0; f--) {
-                if (f > 1) {
-                    newFriendParagraph.innerHTML += f + " lines of code in the file, " + f +
-                    " lines of code. " + name + " strikes one out, clears it all out " + (f - 1) +
-                    " lines of code in the file. " + "</br>" ;
-                } else {
-                    newFriendParagraph.innerHTML += " 1 line of code in the file, 1 line of code. " + name
-                    + " strikes it out, clears it all out, 0 lines of code in the file. " + "</br>";
+                document.body.appendChild(newFriendDiv);
+                let newFriendHeader = document.createElement("h3");
+                
+                
+                newFriendDiv.setAttribute('class', 'friend');
+                newFriendHeader.innerText = name;
+                newFriendDiv.className = "friend";
+                newFriendDiv.appendChild(newFriendHeader);
+                
+                
+                for (j = 99; j > 0; j--) {
+                    if (j > 2) {
+                        let lyric = document.createElement("p");
+                        
+                        lyric.innerText = (j + ' lines of code in the file, ' + j + ' lines of code. ' + name +
+                        ' strikes one out, clears it all out, ' + (j - 1) + ' lines of code in the file.')
+                        newFriendDiv.appendChild(lyric)
+                    }
+                    else if (j == 2) {
+                        let lyric = document.createElement("p");
+                        
+                        lyric.innerText = (j + ' lines of code in the file, ' + j +
+                        ' lines of code. ' + name + ' strikes one out, clears it all out, ' + (j - 1) +
+                        ' line of code in the file.')
+                        newFriendDiv.appendChild(lyric)
+                        
+                    } else {
+                        let lyric = document.createElement("p");
+                        
+                        lyric.innerText = (j + ' line of code in the file,' + j + ' line of code. ' + name
+                        + ' strikes it out, clears it all out, 0 lines of code in the file. ')
+                        newFriendDiv.appendChild(lyric)
+                    }
                 }
-            }
-            body.appendChild(newFriendDiv);
-        });
+            });
+        
     });
+  document.body.appendChild(sing)
 });
-    
+
+
+
+
+/*newFriendParagraph.innerHTML += f + " lines of code in the file, " + f +
+" lines of code. " + name + " strikes one out, clears it all out, " + (f - 1) +
+" lines of code in the file. " + "</br>" ; */
 /*sing.addEventListener("click", function () {
          for (let f = 0; f < friends.length; f++) {
              for (let j = 99; j > 0; j--)
